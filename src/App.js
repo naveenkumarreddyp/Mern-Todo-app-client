7823db8c0a73edf98f5adc6ac33319e90bdef995
naveenkumarreddyp <92851599+naveenkumarreddyp@users.noreply.github.com>
2022-11-20 21:04:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import TodoList from "./components/TodoList";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export const URL = process.env.REACT_APP_SERVER_URL
+export const URL = process.env.REACT_APP_SERVER_URL || "https://todo-app-api-hemx.onrender.com"
 
 function App() {
   return (
